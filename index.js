6bba2c06198b87776565bbc6413fe133fd518ad4
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-04-28 01:16:08 +0600

    job data post, get and delete to spacefic route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { databaseConnection } = require("./db/database-connect");
+const { ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,6 +34,29 @@ app.post("/add-job", async (req, res) => {
   const jobData = req.body;
   // console.log(jobData);
   const result = await jobsCollection.insertOne(jobData);
+  res.send(result);
+});
+
+// Get all jobs in the UI
+app.get("/jobs", async (req, res) => {
+  const result = await jobsCollection.find().toArray();
+  res.send(result);
+});
+
+// Get all jobs by specific user
+app.get("/jobs/:email", async (req, res) => {
+  const email = req.params.email;
+  const query = { "buyer.email": email };
+  const result = await jobsCollection.find(query).toArray();
+  res.send(result);
+  // console.log(result);
+});
+
+// delete posted data
+app.delete("/job/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await jobsCollection.deleteOne(query);
   res.send(result);
 });
 
