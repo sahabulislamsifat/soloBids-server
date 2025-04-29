c766214f25f0919b3ac248acae270fb0f8d0e9e7
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-04-30 02:42:32 +0600

    secure some route by custom hooks and added jwt authentication

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 const { databaseConnection } = require("./db/database-connect");
 const { ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,15 +17,60 @@ databaseConnection();
 const corsOptions = {
   origin: ["http://localhost:5173"],
   credentials: true,
+  optionalSuccessStatus: 200,
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cookieParser());
+
+// Verify token middleware
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) return res.status(401).send({ message: "unauthorized access" });
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+  });
+
+  next();
+};
 
 // Database Collection
 const db = require("./db/database-connect").client.db("solo-bids");
 const jobsCollection = db.collection("jobs");
 const bidsCollection = db.collection("bids");
+
+// Generate Json Web Token
+app.post("/jwt", async (req, res) => {
+  const email = req.body;
+  // create token
+  const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "365d",
+  });
+  console.log(token);
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ success: true });
+});
+
+// logout || clear cookie from browser
+app.get("/logout", async (req, res) => {
+  res
+    .clearCookie("token", {
+      maxAge: 0,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({ success: true });
+});
 
 // Welcome Route
 app.get("/", async (req, res) => {
@@ -46,16 +93,19 @@ app.get("/jobs", async (req, res) => {
 });
 
 // Get all jobs by specific user
-app.get("/jobs/:email", async (req, res) => {
+app.get("/jobs/:email", verifyToken, async (req, res) => {
   const email = req.params.email;
+  const decodedEmail = req.user?.email;
+  if (decodedEmail !== email)
+    return res.status(401).send({ message: "unauthorized access" });
   const query = { "buyer.email": email };
   const result = await jobsCollection.find(query).toArray();
   res.send(result);
   // console.log(result);
 });
 
-// delete posted data
-app.delete("/job/:id", async (req, res) => {
+// delete a job from database
+app.delete("/job/:id", verifyToken, async (req, res) => {
   const id = req.params.id;
   const query = { _id: new ObjectId(id) };
   const result = await jobsCollection.deleteOne(query);
@@ -111,9 +161,12 @@ app.post("/add-bid", async (req, res) => {
 });
 
 // get all bids for a specific user
-app.get("/bids/:email", async (req, res) => {
+app.get("/bids/:email", verifyToken, async (req, res) => {
   const isBuyer = req.query.buyer;
   const email = req.params.email;
+  const decodedEmail = req.user?.email;
+  if (decodedEmail !== email)
+    return res.status(401).send({ message: "unauthorized access" });
   let query = {};
   if (isBuyer) {
     query.buyer = email;
@@ -135,6 +188,24 @@ app.patch("/update-bid-status/:id", async (req, res) => {
     $set: { status },
   };
   const result = await bidsCollection.updateOne(filter, update);
+  res.send(result);
+});
+
+// get all jobs
+app.get("/all-jobs", async (req, res) => {
+  const filter = req.query.filter;
+  const search = req.query.search;
+  const sort = req.query.sort;
+  let options = {};
+  if (sort) options = { sort: { deadline: sort === "asc" ? 1 : -1 } };
+  let query = {
+    job_title: {
+      $regex: search,
+      $options: "i",
+    },
+  };
+  if (filter) query.category = filter;
+  const result = await jobsCollection.find(query, options).toArray();
   res.send(result);
 });
 
